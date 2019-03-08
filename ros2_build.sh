f82dbfa31ea1b547cbf101e6b555f49904cb1353
Juan Rodriguez Hortala <hortala@amazon.com>
2019-03-08 10:35:01 -0800

    add missing variable expansion

--- ros2_build.sh
+++ ros2_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+PACKAGES_WITH_EXTRA_TEST_BUILD_TARGET=($PACKAGES_WITH_EXTRA_TEST_BUILD_TARGET)
+
 # install dependencies
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
 echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list
